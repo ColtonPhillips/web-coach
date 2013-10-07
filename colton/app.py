88901e9ae1241dff07aca2992bdebedb3d462545
beyamor <beyamor@gmail.com>
2013-10-07 12:46:54 -0700

    Started displaying projects

--- colton/app.py
+++ colton/app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+PROJECTS = {
+		"Corvus": {
+			"link": "www.google.com"
+			}
+		}
+
 @app.route("/")
 def hello():
-    return render_template("hello.html")
+	return render_template("hello.html", projects=PROJECTS)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)

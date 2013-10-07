a68f3e4aaeee5cf158e40ef4225e52701eeb44b4
beyamor <beyamor@gmail.com>
2013-10-07 13:05:40 -0700

    External links

--- colton/app.py
+++ colton/app.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+SIDEBAR_LINKS = [
+		("TWITTER", "https://twitter.com/ColtonPhillips"),
+		("GITHUB", "https://github.com/ColtonPhillips"),
+		("SOUNDCLOUD", "https://soundcloud.com/coltonphillips"),
+		("YOUTUBE", "http://www.youtube.com/user/ColtonPhillips")
+		]
+
 PROJECTS = {
-		"Corvus": {
-			"link": "www.google.com"
+		"Some Game": {
+			"link": "http://www.whatever.com",
+			"description": "A game about a thing."
 			}
 		}
 
 @app.route("/")
 def hello():
-	return render_template("hello.html", projects=PROJECTS)
+	return render_template("hello.html", projects=PROJECTS, links=SIDEBAR_LINKS)
 
 if __name__ == "__main__":
 	app.run(debug=True)

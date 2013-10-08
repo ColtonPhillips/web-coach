50218421e6ab2679cc0043620ac914af511ab25a
beyamor <beyamor@gmail.com>
2013-10-07 19:18:04 -0700

    Styling + image names

--- colton/app.py
+++ colton/app.py
@@ -8,7 +8,12 @@ GALLERY_PATH = str(app.root_path) + "/static/images/gallery"
 
 @app.route("/gallery")
 def gallery():
-	pictures = ["/static/images/gallery/" + picture for picture in os.listdir(GALLERY_PATH)]
+	pictures = []
+	for image_file in os.listdir(GALLERY_PATH):
+		source = "/static/images/gallery/" + image_file
+		name, _ = os.path.splitext(image_file)
+		pictures.append((name, source))
+
 	return render_template("gallery.html", pictures=pictures, links=SIDEBAR_LINKS)
 
 @app.route("/")

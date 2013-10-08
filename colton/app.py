f1d339df3c89103fe2b7f89e24f89ec6843c1fd9
beyamor <beyamor@gmail.com>
2013-10-07 19:27:14 -0700

    Favicon and title, bro

--- colton/app.py
+++ colton/app.py
@@ -1,10 +1,15 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from content.links import SIDEBAR_LINKS
 from content.projects import PROJECTS
 app = Flask(__name__)
 
-GALLERY_PATH = str(app.root_path) + "/static/images/gallery"
+GALLERY_PATH = os.path.join(app.root_path, 'static', 'images', 'gallery')
+
+@app.route('/favicon.ico')
+def favicon():
+	    return send_from_directory(os.path.join(app.root_path, 'static', 'images'),
+			                                   'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route("/gallery")
 def gallery():
@@ -21,4 +26,4 @@ def main():
 	return render_template("main.html", projects=PROJECTS, links=SIDEBAR_LINKS)
 
 if __name__ == "__main__":
-	app.run(debug=False)
+	app.run(debug=True)

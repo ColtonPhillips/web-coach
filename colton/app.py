0ebd79649aeac7cda87ada423c55c2c086f4a10e
beyamor <beyamor@gmail.com>
2013-10-18 12:05:02 -0700

    Defining galleries

--- colton/app.py
+++ colton/app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, send_from_directory
 from definitions import all_definitions
+import gallery
 
 app = Flask(__name__)
 
@@ -8,22 +9,13 @@ app = Flask(__name__)
 # we need to add them to the jina environment
 app.jinja_env.globals.update(**all_definitions)
 
-GALLERY_PATH = os.path.join(app.root_path, 'static', 'images', 'gallery')
-
 @app.route('/favicon.ico')
 def favicon():
 	    return send_from_directory(os.path.join(app.root_path, 'static', 'images'),
 			    'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route("/gallery")
-def gallery():
-	pictures = []
-	for image_file in os.listdir(GALLERY_PATH):
-		source = "/static/images/gallery/" + image_file
-		name, _ = os.path.splitext(image_file)
-		pictures.append((name, source))
-
-	return render_template("gallery.html", pictures=pictures)
+GALLERY_PATH = os.path.join(app.root_path, 'static', 'images', 'gallery')
+gallery.define(app, "Main Gallery", "/gallery", GALLERY_PATH)
 
 @app.route("/")
 def main():

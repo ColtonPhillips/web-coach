25eec6d12938b3bde9e25483d2eb520fdf1cad8c
Colton Phillips <coltonjphillips@gmail.com>
2014-03-23 16:18:53 -0700

    default message: fast commit

--- colton/app.py
+++ colton/app.py
@@ -53,10 +53,10 @@ def adipose_suggest_high_score():
 		if is_number(score):
 			if score > high_score:
 
-				return request.form["team"] + os.linesep + request.form["score"] 
 				with open(ADIPOSE_SCORE_PATH, "r+") as _f:
 					lines = _f.readlines()
 				        f.seek(0)
+				return request.form["team"] + os.linesep + request.form["score"] 
 					f.write(request.form["team"]+ os.linesep)
 					f.write(request.form["score"]+ os.linesep)
 				        f.writelines(lines)

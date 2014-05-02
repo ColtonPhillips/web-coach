78a9b23373f184aa0e130bb631baad3861f7b387
Colton Phillips <coltonjphillips@gmail.com>
2014-05-01 22:21:29 -0700

    default message: fast commit

--- colton/app.py
+++ colton/app.py
@@ -16,8 +16,8 @@ def favicon():
 
 SKETCHES_PATH = os.path.join(app.root_path, 'static', 'images', 'sketches')
 PIXELS_PATH = os.path.join(app.root_path, 'static','images','pixels')
-gallery.define(app, "sketches", "/sketches", SKETCHES_PATH)
-gallery.define(app, "pixels", "/pixels", PIXELS_PATH)
+gallery.define(app, "my sketches", "/sketches", SKETCHES_PATH)
+gallery.define(app, "my pixels", "/pixels", PIXELS_PATH)
 
 @app.route("/")
 def main():
--- colton/gallery.py
+++ colton/gallery.py
@@ -5,10 +5,10 @@ def define(app, gallery_name, route, image_folder):
 	def gallery():
 		pictures = []
 		for image_file in os.listdir(image_folder):
-			source = "/static/images/" + gallery_name + "/"  + image_file
+			source = "/static/images/sketches" + image_file
 			name, _ = os.path.splitext(image_file)
 			pictures.append((name, source))
 
-		return render_template(gallery_name+".html", pictures=pictures)
+		return render_template("sketches.html", pictures=pictures)
 
 	app.add_url_rule(route, gallery_name, gallery)

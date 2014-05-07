7194711f37e5704f646b741ade2a159fca056527
Colton Phillips <coltonjphillips@gmail.com>
2014-05-06 17:08:10 -0700

    default message: fast commit

--- colton/definitions.py
+++ colton/definitions.py
@@ -21,7 +21,7 @@ project_images = "/static/images/projects/"
 projects = [{
 	"title": "SPONSOR: Justin Smith",
 	"link": "http://captaingames.itch.io/",
-		"description": "Justin Smith has been making the World's greatest video games since before you were born.",
+		"description": "Justin Smith has been making the World's Greatest Video Games since before you were born.",
 		"thumbnail":  project_images + "justinsmith-thumb.gif",
 		"header": project_images + "justinsmith-header.png",
 	}, {

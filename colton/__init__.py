1ce01979cd727a1954d75a6c404cc1936bd39cf4
Colton Phillips <coltonjphillips@gmail.com>
2014-06-26 16:56:30 -0700

    default message: fast commit

--- colton/__init__.py
+++ colton/__init__.py
@@ -1,9 +1,10 @@
 from app import app
 
 def get_random_status():
-	with open('/static/statuses.txt', 'r') as statusFile:
-		statuses = statusFile.read().split()
-	from random import choice
-	return str(choice(statuses))
+#	with open('/static/statuses.txt', 'r') as statusFile:
+#		statuses = statusFile.read().split()
+#	from random import choice
+#	return str(choice(statuses))
+	return "Hello Workd!"
 
 app.jinja_env.globals.update(get_random_status=get_random_status)

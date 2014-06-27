17639b2bf256fdc5ca95557ad0eba17b7ee59580
Colton Phillips <coltonjphillips@gmail.com>
2014-06-26 18:11:47 -0700

    default message: fast commit

--- colton/statuses.py
+++ colton/statuses.py
@@ -1,4 +1,18 @@
 statuses = [
-"Hello world!",
+"This feature took me way too long to implement",
+"Hamburgers",
+"So rando lol Much statuses",
+"My good pal Tom helped make this cool site",
+"This website uses the Javiest of Javascripts",
+"Brandon Duncan made me this way",
+"I'm not joking.",
+"This is not a test",
+"Just kidding!",
+"Here I am, Senior Internet User. Take me.",
+"Welcome to the frump page of the Internet",
+"I only do everything",
+"What? Me worry?",
+"One Million! How many can I give you!",
+"Hello World!",
 "HI I'M COLTON PHILLIPS AND I LIKE THINGS REAL GOOD"
 ]

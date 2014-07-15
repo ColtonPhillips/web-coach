5ab0d25bc9d733b568c8b5df795a366c6dc09051
Colton Phillips <coltonjphillips@gmail.com>
2014-07-15 15:01:10 -0600

    default message: fast commit

--- colton/test.py
+++ colton/test.py
@@ -4,6 +4,6 @@ def define(app):
 	@app.route("/test", methods = ['GET', 'POST'])
 	def test():
 		if request.method == 'POST':	
-			return render_template("boiler.html", bbb=request.data)
+			return render_template("test.html", bbb=request.data)
 		else: 
 			return render_template("test.html")

4c9474910f82dbb17c66c18b96bd2bb96780a3dc
Colton Phillips <coltonjphillips@gmail.com>
2014-07-15 14:46:57 -0600

    default message: fast commit

--- colton/test.py
+++ colton/test.py
@@ -1,12 +1,15 @@
 from flask import render_template
 
 def define(app):
-#	@app.route("/test")
+	@app.route("/test")
 	def test():
+		return render_template("test.html")
+	"""
 		if request.method == 'POST':	
 			return render_template("boiler.html")
 		else: 
 			return render_template("test.html")
 		return render_template("test.html")
 
-	app.add_url_rule("/test", "test", test)
+	"""
+#	app.add_url_rule("/test", "test", test)

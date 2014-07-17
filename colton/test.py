0b18a894d857fc90cd5bfaa857285eeeb20189ef
Colton Phillips <coltonjphillips@gmail.com>
2014-07-16 19:40:46 -0600

    default message: fast commit

--- colton/test.py
+++ colton/test.py
@@ -1,13 +1,14 @@
 from flask import render_template, request
 import os
-from prepender import Prepender
+from neato import Prepender
 
 def define(app):
 	@app.route("/test", methods = ['GET', 'POST'])
 	def test():
 		# append new note
-#		if request.method == 'POST':	
-#			with Prepender(full_path) as notesFile:
+		if request.method == 'POST':	
+			with Prepender(full_path) as notesFile:
+				notesFile.write("testing")
 #				notesFile.write(request.form.note)
 	
 		#get existing notes

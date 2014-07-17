36f984f389d97eff17a76f0c4e299842e8da4006
Colton Phillips <coltonjphillips@gmail.com>
2014-07-16 20:52:29 -0600

    default message: fast commit

--- colton/test.py
+++ colton/test.py
@@ -7,9 +7,9 @@ def define(app):
 	def test():
 		full_path = os.path.join(app.root_path, "static", "notes.txt")
 		# append new note
-		if request.method == 'POST':	
-			with Prepender(full_path) as notesFile:
-				pass
+#		if request.method == 'POST':	
+#			with Prepender(full_path) as notesFile:
+#				pass
 #				notesFile.write(str(request.form.note) + "\n===\n")
 	
 		#get existing notes

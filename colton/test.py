51301e57dbd65607ff784dc9dd2edf8d1b6fd8a6
Colton Phillips <coltonjphillips@gmail.com>
2014-07-16 20:57:27 -0600

    default message: fast commit

--- colton/test.py
+++ colton/test.py
@@ -17,27 +17,10 @@ def define(app):
 		with open(full_path,'r') as notesFile:
 			notes = notesFile.read().split("===")
 		if request.method == 'POST':
-			notes.append(str(request.form.note) + "sssss" )
+			notes.append(request.form.note)
 
 		# just added a note
 		if request.method == 'POST':	
 			return render_template("test.html", notes=notes, form=request.form)
 		else: 
 			return render_template("test.html", notes=notes)
-"""
-import os
-from flask import render_template
-
-def define(app, gallery_name, route, gallery_folder):
-	def gallery():
-		pictures = []
-		full_path = os.path.join(app.root_path, "static", "images", gallery_folder)
-		for image_file in os.listdir(full_path):
-			source = "/static/images/" + gallery_folder + "/" + image_file
-			name, _ = os.path.splitext(image_file)
-			pictures.append((name, source))
-
-		return render_template("gallery.html", pictures=pictures)
-
-	app.add_url_rule(route, gallery_name, gallery)
-"""

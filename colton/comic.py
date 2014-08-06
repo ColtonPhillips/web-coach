ffe4df84329a4ab720cb2f88a7bc37b75d00f371
Colton Phillips <coltonjphillips@gmail.com>
2014-08-05 19:31:17 -0600

    default message: fast commit

--- colton/comic.py
+++ colton/comic.py
@@ -7,7 +7,7 @@ def define(app):
 	def comic(panel_id="1"):
 		full_path = os.path.join(app.root_path, "static", "comic", (panel_id + ".png"))
 		if(os.path.isfile(full_path)):
-			return render_template("comic.html", panel_src="/static/comic/1.png")
+			my_src = "/static/comic/" + panel_id + ".png"
+			return render_template("comic.html", panel_src=my_src)
 		else:
-			full_path = os.path.join(app.root_path, "static", "comic", "1.png")
-			return render_template("comic.html", panel_src=full_path)
+			return render_template("comic.html", panel_src="/static/comic/1.png")

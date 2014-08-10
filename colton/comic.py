0a2d1862901411e20788c0231ecc207edf4614f0
Colton Phillips <coltonjphillips@gmail.com>
2014-08-09 21:51:42 -0600

    default message: fast commit

--- colton/comic.py
+++ colton/comic.py
@@ -3,7 +3,7 @@ import os, glob
  
 def define(app):
 	def comic_path(panel_id):
-		return "/static/comic/" + str(panel_id) + ".png"
+		return "/static/comic/" + str(panel_id) + ".jpg"
 	app.jinja_env.globals.update(comic_path=comic_path)
 
 	def comic_url(panel_id):
@@ -19,13 +19,13 @@ def define(app):
 			if (not panel_id.isdigit()):
 				panel_id = 1
 		my_path = os.path.join(app.root_path, "static", "comic")
-		png_count = len(glob.glob1(my_path,"*.png"))
+		png_count = len(glob.glob1(my_path,"*.jpg"))
 		if (int(panel_id) > png_count):
 			panel_id = 1
 		if (int(panel_id) < 1):
 			panel_id = png_count	
 
-		full_path = os.path.join(app.root_path, "static", "comic", (str(panel_id) + ".png"))
+		full_path = os.path.join(app.root_path, "static", "comic", (str(panel_id) + ".jpg"))
 		return render_template("comic.html", panel_id=panel_id)
 		if(os.path.isfile(full_path)):
 			return render_template("comic.html", panel_id=panel_id)

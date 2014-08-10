9eb040b081493b4ced92c4222b8db4731ef36dc6
Colton Phillips <coltonjphillips@gmail.com>
2014-08-09 18:29:22 -0600

    default message: fast commit

--- colton/comic.py
+++ colton/comic.py
@@ -2,13 +2,17 @@ from flask import render_template, request
 import os
 
 def define(app):
+	def comic_path(panel_id):
+		return "/static/comic/" + panel_id + ".png"
+ 
+	app.jinja_env.globals.update(comic_path=comic_path)
+
 	@app.route("/comic")
 	@app.route("/comic/")
 	@app.route("/comic/<panel_id>")
 	def comic(panel_id="1"):
 		full_path = os.path.join(app.root_path, "static", "comic", (panel_id + ".png"))
 		if(os.path.isfile(full_path)):
-			my_src = "/static/comic/" + panel_id + ".png"
-			return render_template("comic.html", panel_src=my_src)
+			return render_template("comic.html", panel_id=panel_id)
 		else:
-			return render_template("comic.html", panel_src="/static/comic/1.png")
+			return render_template("comic.html", panel_id=1)

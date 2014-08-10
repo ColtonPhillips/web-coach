d8714088dbca1671aa5cb646c950c607238a3be5
Colton Phillips <coltonjphillips@gmail.com>
2014-08-09 18:46:41 -0600

    default message: fast commit

--- colton/comic.py
+++ colton/comic.py
@@ -6,6 +6,8 @@ def define(app):
 	@app.route("/comic/")
 	@app.route("/comic/<panel_id>")
 	def comic(panel_id=1):
+		return render_template("boiler.html")
+
 		full_path = os.path.join(app.root_path, "static", "comic", (str(panel_id) + ".png"))
 		if(os.path.isfile(full_path)):
 			return render_template("comic.html", panel_id=panel_id)

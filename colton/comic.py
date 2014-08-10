d21e8a5086cbdb475955c15b9bd3ff69a90749f6
Colton Phillips <coltonjphillips@gmail.com>
2014-08-09 20:48:34 -0600

    default message: fast commit

--- colton/comic.py
+++ colton/comic.py
@@ -6,10 +6,20 @@ def define(app):
 		return "/static/comic/" + str(panel_id) + ".png"
 	app.jinja_env.globals.update(comic_path=comic_path)
 
+	def comic_url(panel_id):
+		return "http://www.coltonphillips.ca/comic/" + str(panel_id)
+	app.jinja_env.globals.update(comic_url=comic_url)
+
+
 	@app.route("/comic")
 	@app.route("/comic/")
 	@app.route("/comic/<panel_id>")
 	def comic(panel_id=1):
+		if (panel_id < 1):
+			panel_id = 1
+		if (panel_id > 100):
+			panel_id = 1
+
 		full_path = os.path.join(app.root_path, "static", "comic", (str(panel_id) + ".png"))
 		return render_template("comic.html", panel_id=panel_id)
 		if(os.path.isfile(full_path)):

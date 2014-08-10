9dd354e6b553fe106e3f358d399a53c4d0bc7b5b
Colton Phillips <coltonjphillips@gmail.com>
2014-08-09 19:11:55 -0600

    default message: fast commit

--- colton/comic.py
+++ colton/comic.py
@@ -1,13 +1,15 @@
 from flask import render_template, request
 import os
-
+def ia(panel_id):
+	return "/static/comic/" + str(panel_id) + ".png"
+ 
 def define(app):
 	@app.route("/comic")
 	@app.route("/comic/")
 	@app.route("/comic/<panel_id>")
 	def comic(panel_id=1):
 		full_path = os.path.join(app.root_path, "static", "comic", (str(panel_id) + ".png"))
-		return render_template("comic.html", shit=2)
+		return render_template("comic.html", panel_src=ia(panel_id))
 		if(os.path.isfile(full_path)):
 			return render_template("comic.html", panel_id=panel_id)
 		else:

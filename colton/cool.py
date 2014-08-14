814b8d060df6c21a145259b75471f9c6c6e00764
Colton Phillips <coltonjphillips@gmail.com>
2014-08-14 16:10:10 -0600

    default message: fast commit

--- colton/cool.py
+++ colton/cool.py
@@ -8,17 +8,11 @@ def define(app):
 		# Lazy, not pefect way to solve it
 		cool_date = request.cookies.get('day_of_month')
 		if cool_date is None:
-			snoog = str(cool_date) + str('a')
-			neato.log_chince(snoog)
 			resp = make_response(render_template("cool.html"))
 			resp.set_cookie('day_of_month', str(datetime.now().month))
 			return resp
 		else:
-			if (cool_date == datetime.now().month):
-				snoog = str(cool_date) + str('b')
-				neato.log_chince(snoog)
+			if (cool_date == str(datetime.now().month)):
 				return "same day"
 			else:
-				snoog = str(cool_date) + str('c')
-				neato.log_chince(snoog)
 				return "different day"

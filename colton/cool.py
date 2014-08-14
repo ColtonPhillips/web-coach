33dd5aac7cc9cf28fbeb2b00f2c3c294b718d054
Colton Phillips <coltonjphillips@gmail.com>
2014-08-14 15:44:54 -0600

    default message: fast commit

--- colton/cool.py
+++ colton/cool.py
@@ -7,6 +7,7 @@ def define(app):
 	def cool():
 		# Lazy, not pefect way to solve it
 		cool_date = request.cookies.get('day_of_month')
+		return "test"
 		if cool_date is None:
 			resp = make_response(render_template("cool.html"))
 			resp.set_cookie('day_of_month', datetime.now().month)

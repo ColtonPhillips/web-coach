3738c44ae0b29fae9a5239eae460b853bec5f344
Colton Phillips <coltonjphillips@gmail.com>
2014-08-14 16:15:08 -0600

    default message: fast commit

--- colton/cool.py
+++ colton/cool.py
@@ -5,14 +5,15 @@ from datetime import datetime
 def define(app):
 	@app.route("/cool")
 	def cool():
-		# Lazy, not pefect way to solve it
-		cool_date = request.cookies.get('day_of_month')
+		day_of_year = str(datetime.now().month) + str(datetime.now().day)
+		cool_date = request.cookies.get('day_of_year')
 		if cool_date is None:
 			resp = make_response(render_template("cool.html"))
-			resp.set_cookie('day_of_month', str(datetime.now().month))
+			resp.set_cookie('day_of_year', day_of_year)
 			return resp
 		else:
-			if (cool_date == str(datetime.now().month)):
+			if (cool_date == day_of_year): 
+				# You can only vote one time a day
 				return "same day"
 			else:
 				return "different day"

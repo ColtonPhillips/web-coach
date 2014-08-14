3b408c0e3e73a060a546d7c00853508e51b3dc5c
Colton Phillips <coltonjphillips@gmail.com>
2014-08-14 15:51:05 -0600

    default message: fast commit

--- colton/cool.py
+++ colton/cool.py
@@ -5,11 +5,11 @@ from datetime import datetime
 def define(app):
 	@app.route("/cool")
 	def cool():
-		return "oh god i just wish things would load faster"
 		# Lazy, not pefect way to solve it
 		cool_date = request.cookies.get('day_of_month')
-		neato.chince_log(cool_date)
 		return "test"
+
+		neato.chince_log(cool_date)
 		if cool_date is None:
 			resp = make_response(render_template("cool.html"))
 			resp.set_cookie('day_of_month', datetime.now().month)

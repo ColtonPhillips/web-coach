be9e426a7f64be20da75190e47da0d1ac54a4ce3
Colton Phillips <coltonjphillips@gmail.com>
2014-08-14 15:58:01 -0600

    default message: fast commit

--- colton/cool.py
+++ colton/cool.py
@@ -8,8 +8,6 @@ def define(app):
 		# Lazy, not pefect way to solve it
 		cool_date = request.cookies.get('day_of_month')
 		neato.log_chince(cool_date)
-		return "test"
-
 		if cool_date is None:
 			resp = make_response(render_template("cool.html"))
 			resp.set_cookie('day_of_month', datetime.now().month)

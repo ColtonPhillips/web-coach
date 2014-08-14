27e80b8842644c390f93834ec1c472e0cb19da9b
Colton Phillips <coltonjphillips@gmail.com>
2014-08-14 15:06:02 -0600

    default message: fast commit

--- colton/cool.py
+++ colton/cool.py
@@ -4,8 +4,9 @@ import neato
 def define(app):
 	@app.route("/cool")
 	def cool():
-#		meh = request.cookies.get('day_of_year')
-#		neato.log_chince(meh)
+		meh = request.cookies.get('day_of_year')
+		neato.log_chince(meh)
+		return 'mad'
 		resp = make_response(render_template("cool.html"))
 		resp.set_cookie('day_of_the_year', "today!")
 		neato.log_chince(resp)

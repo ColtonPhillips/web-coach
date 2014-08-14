efda723bcae25e6e4bab3fcca63439b073d6be71
Colton Phillips <coltonjphillips@gmail.com>
2014-08-14 14:54:55 -0600

    default message: fast commit

--- colton/cool.py
+++ colton/cool.py
@@ -1,8 +1,13 @@
-from flask import render_template
+from flask import render_template, request, make_response
 import neato
 
 def define(app):
 	@app.route("/cool")
 	def cool():
+		meh = request.cookies.get('day_of_year')
+#		resp = make_response(render_template("cool.html"))
+#		resp.set_cookie('day_of_the_year', "today!")
+#		return resp
+		return meh
 		return render_template("cool.html")
 

f7e321484406090e527289de8dc94a1a5cf2e2ec
Colton Phillips <coltonjphillips@gmail.com>
2014-08-13 18:33:59 -0600

    default message: fast commit

--- colton/cool.py
+++ colton/cool.py
@@ -7,7 +7,7 @@ def define(app):
 	def cool():
 		import os
 		log_path = os.path.join(app.root_path, "log", "chince.txt")
-		with open(log_path, "r+") as _f:
+		with open(log_path, "a") as _f:
 			_f.write("god fucking damnit")
 		app.logger.warning("GAEEE!")
 		return render_template("cool.html")

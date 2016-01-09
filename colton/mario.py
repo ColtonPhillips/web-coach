db13c1b75960e10f2529d6c8a9edcc83edaa480c
Colton Phillips <coltonjphillips@gmail.com>
2016-01-09 10:31:35 -0800

    default message: fast commit

--- colton/mario.py
+++ colton/mario.py
@@ -1,13 +1,15 @@
 import urllib2, re, random
 from flask import render_template
+from bs4 import BeautifulSoup
 
 def define(app):
 	@app.route("/mario")
 	def mario():	
 		url = "https://supermariomakerbookmark.nintendo.net/profile/ColtonPhillips"	
 		data = urllib2.urlopen(url).read()
-		data1 = data
 		courses = re.findall('href="/courses/(.*?)"',data, re.DOTALL)
-		names = re.findall('class="course-title">(.*?)</div>',data1, re.DOTALL)	
+		#names = re.findall('<div class="course-title">(.*?)</div>',data1, re.DOTALL)	
+        soup = BeautifulSoup(data.read(), from_encoding=data.info().getparam('charset'))
+        names = soup.find_all('course-title')
 		#return render_template("mario.html", levels=zip(courses, names))
 		return render_template("mario.html", levels=courses, names=names)

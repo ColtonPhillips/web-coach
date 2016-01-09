b6a946bc43e20142a5d16bb4a7192ad56ec8cc34
Colton Phillips <coltonjphillips@gmail.com>
2016-01-09 09:58:43 -0800

    default message: fast commit

--- colton/mario.py
+++ colton/mario.py
@@ -4,8 +4,9 @@ from flask import render_template
 def define(app):
 	@app.route("/mario")
 	def mario():	
-	    url = "https://supermariomakerbookmark.nintendo.net/profile/ColtonPhillips"	
-	    data = urllib2.urlopen(url).read()
-	    courses = re.findall('href="/courses/(.*?)"',data , re.DOTALL)
-	    names = re.findall('<div class="course-title">(.*?)</div>',data , re.DOTALL)
-	    return render_template("mario.html", levels=zip(courses, names))
+		url = "https://supermariomakerbookmark.nintendo.net/profile/ColtonPhillips"	
+		data = urllib2.urlopen(url).read()
+		data1 = data
+		courses = re.findall('href="/courses/(.*?)"',data, re.DOTALL)
+		names = re.findall('class="course-title">(.*?)</div>',data1, re.DOTALL)
+		return render_template("mario.html", levels=zip(courses, names))

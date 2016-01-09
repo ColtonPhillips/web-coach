c805d9c550be30fa9b6c65b0f52f41ecd170a2bf
Colton Phillips <coltonjphillips@gmail.com>
2016-01-09 10:13:13 -0800

    default message: fast commit

--- colton/mario.py
+++ colton/mario.py
@@ -9,6 +9,5 @@ def define(app):
 		data1 = data
 		courses = re.findall('href="/courses/(.*?)"',data, re.DOTALL)
 		names = re.findall('class="course-title">(.*?)</div>',data1, re.DOTALL)	
-		names = ["a","b","c"]
 		#return render_template("mario.html", levels=zip(courses, names))
 		return render_template("mario.html", levels=courses, names=names)

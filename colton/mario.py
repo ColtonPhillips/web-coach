711633e0ffd133dbc3a1c79d922451e3f76df78d
Colton Phillips <coltonjphillips@gmail.com>
2016-01-09 09:17:08 -0800

    default message: fast commit

--- colton/mario.py
+++ colton/mario.py
@@ -1,13 +1,11 @@
-
-
 import urllib2, re, random
 from flask import render_template
 
 def define(app):
 	@app.route("/mario")
-	def mario():
-        url = "https://supermariomakerbookmark.nintendo.net/profile/ColtonPhillips"
-        data = urllib2.urlopen(url).read()
-        levels = re.findall('href="/courses/(.*?)"',data , re.DOTALL)
-		random.shuffle(levels)
-		return render_template("mario.html", levels=levels)
+	def mario():	
+	url = "https://supermariomakerbookmark.nintendo.net/profile/ColtonPhillips"	
+	data = urllib2.urlopen(url).read()
+	levels = re.findall('href="/courses/(.*?)"',data , re.DOTALL)
+	random.shuffle(levels)
+	return render_template("mario.html", levels=levels)

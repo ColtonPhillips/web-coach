a5a9927f61bc4e7c6ad02a41d32ff4761663a591
Colton Phillips <coltonjphillips@gmail.com>
2017-04-27 03:55:36 -0700

    default message: fast commit

--- colton/app.py
+++ colton/app.py
@@ -34,6 +34,26 @@ cool.define(app)
 # Colton is cooler because Brandon wears glasses
 mario.define(app)
 
+def check_auth(username,password):
+        return username == "admin" and password == "secret"
+
+def authenticate():
+        return Response(
+         'Could not verify your access level for that URL.\n'
+         'You have to login with proper credentials', 401,
+         {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+                auth = request.authorization
+                if not auth or not check_auth(auth.username, auth.password):
+                        return authenticate()
+                return f(*args, **kwargs)
+        return decorated
+
+
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static', 'images'),
